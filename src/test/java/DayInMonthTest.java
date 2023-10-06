7899d79ca555a1c8d831f73fac3e66b5881b2645
votrongluan <trongluan115@gmail.com>
2023-10-06 08:19:28 +0700

    new feature

--- src/test/java/DayInMonthTest.java
+++ src/test/java/DayInMonthTest.java
@@ -1,34 +1,35 @@
 import main.DateTimeChecker;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DayInMonthTest {
     @Test
     public void testWithMonthHas31Days() {
-        assertEquals(31,DateTimeChecker.dayInMonth(2000, 1));
-        assertEquals(31,DateTimeChecker.dayInMonth(2000, 3));
-        assertEquals(31,DateTimeChecker.dayInMonth(2000, 5));
-        assertEquals(31,DateTimeChecker.dayInMonth(2000, 7));
-        assertEquals(31,DateTimeChecker.dayInMonth(2000, 8));
-        assertEquals(31,DateTimeChecker.dayInMonth(2000, 10));
-        assertEquals(31,DateTimeChecker.dayInMonth(2000, 12));
+        assertEquals(31, DateTimeChecker.dayInMonth(2000, 1));
+        assertEquals(31, DateTimeChecker.dayInMonth(2000, 3));
+        assertEquals(31, DateTimeChecker.dayInMonth(2000, 5));
+        assertEquals(31, DateTimeChecker.dayInMonth(2000, 7));
+        assertEquals(31, DateTimeChecker.dayInMonth(2000, 8));
+        assertEquals(31, DateTimeChecker.dayInMonth(2000, 10));
+        assertEquals(31, DateTimeChecker.dayInMonth(2000, 12));
     }
 
     @Test
     public void testWithMonthHas30Days() {
-        assertEquals(30,DateTimeChecker.dayInMonth(2000, 4));
-        assertEquals(30,DateTimeChecker.dayInMonth(2000, 6));
-        assertEquals(30,DateTimeChecker.dayInMonth(2000, 9));
-        assertEquals(30,DateTimeChecker.dayInMonth(2000, 11));
+        assertEquals(30, DateTimeChecker.dayInMonth(2000, 4));
+        assertEquals(30, DateTimeChecker.dayInMonth(2000, 6));
+        assertEquals(30, DateTimeChecker.dayInMonth(2000, 9));
+        assertEquals(30, DateTimeChecker.dayInMonth(2000, 11));
     }
 
     @Test
     public void testWithMonthHas28Days() {
-        assertEquals(28,DateTimeChecker.dayInMonth(2001, 2));
+        assertEquals(28, DateTimeChecker.dayInMonth(2001, 2));
     }
 
     @Test
     public void testWithMonthHas29Days() {
-        assertEquals(29,DateTimeChecker.dayInMonth(2000, 2));
+        assertEquals(29, DateTimeChecker.dayInMonth(2000, 2));
     }
 }
